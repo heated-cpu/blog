4c5fbe6a6159cf99ea9b2a541d059094c81c836b
ron <ron@net.ops>
2025-07-15 14:22:36 +0800

    汉化版本

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
 	integrations: [tailwind()],
   // site: 'https://heated-cpu.github.io',
-  // base: '/blog/',  
+  // base: '/blog/',
 });
